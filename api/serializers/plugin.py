4b2b91dafb4bcd87d2386916e3c22b80d1d84f53
Samuel/Bagra <samuel67190@gmail.com>
2024-09-08 17:43:49 +0200

    feat: Auth plugin-server instance and give unique token

--- api/serializers/plugin.py
+++ api/serializers/plugin.py
@@ -1,15 +1,15 @@
 from rest_framework import serializers
 from ..models.plugin import Plugin
 
-class PluginSerializer(serializers.HyperlinkedModelSerializer):
+class PluginSerializer(serializers.ModelSerializer):
     class Meta:
         model = Plugin
         fields = ['id', 'name', 'schema']
 
-    def create(self, validated_data):
-        # TODO: creation all table in schema
-        return super().create(validated_data)
+    # def create(self, validated_data):
+    #     # TODO: creation all table in schema
+    #     return super().create(validated_data)
     
-    def update(self, instance, validated_data):
-        # TODO: update all table in schema
-        return super().update(instance, validated_data)
+    # def update(self, instance, validated_data):
+    #     # TODO: update all table in schema
+    #     return super().update(instance, validated_data)
--- api/viewsets/server.py
+++ api/viewsets/server.py
@@ -1,5 +1,13 @@
-from rest_framework import viewsets
+import random, string
+from urllib.parse import urlparse
+
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+
 from ..models.server import Server, ServerPlugins
+from ..models.plugin import Plugin
 from ..serializers.server import ServerSerializer, ServerPluginsSerializer
 
 
@@ -11,3 +19,33 @@ class ServerViewSet(viewsets.ModelViewSet):
 class ServerPluginsViewSet(viewsets.ModelViewSet):
     queryset = ServerPlugins.objects.all()
     serializer_class = ServerPluginsSerializer
+
+    def create(self, request, pk=None):
+        if request.method != 'POST':
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        
+
+        serializer = ServerPluginsSerializer(data=request.data, context={'request': request})
+
+        if serializer.is_valid():
+            server = serializer.data['server']
+            plugin = serializer.data['plugin']
+            
+            try:
+                res = ServerPlugins.objects.get(server=server, plugin=plugin)
+            except ServerPlugins.DoesNotExist:
+                res = None
+
+            if res:
+                token = res.token
+            else:
+                token = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
+                server = get_object_or_404(Server, pk=server)
+                plugin = get_object_or_404(Plugin, pk=plugin)
+
+                obj = ServerPlugins(server=server, plugin=plugin, token=token)
+                obj.save()
+            return Response({'token': token})
+        else:
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
