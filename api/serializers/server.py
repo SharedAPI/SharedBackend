8e4c7ca63f83dc6a14b8a26ad9b1a30cb82cbc51
sladweinRDR <sladwein@roederer.fr>
2025-03-25 16:03:28 +0100

    feat: Add token WIP

--- api/serializers/server.py
+++ api/serializers/server.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 
 from api.models.plugin import Plugin
-from api.models.server import Server
+from api.models.server import Server, ServerPlugins
+
 
 # Serializers define the API representation.
 class ServerSerializer(serializers.ModelSerializer):
@@ -16,5 +17,8 @@ class AddPluginSerializer(serializers.Serializer):
     def create(self, validated_data):
         server = self.context["server"]
         plugin = validated_data["plugin"]
-        server.plugins.add(plugin)
-        return {"server": server.id, "plugin": plugin.id}
+        server_plugin, _ = ServerPlugins.objects.get_or_create(
+            server=server, plugin=plugin
+        )
+
+        return {"server": server_plugin.server, "plugin": server_plugin.plugin}
