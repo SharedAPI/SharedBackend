16b1f33f07f641281fa5cb99a8085e9ce077185c
sladweinRDR <sladwein@roederer.fr>
2025-03-25 13:44:52 +0100

    feat: Add more robust code to handle add_plugin

--- api/utils.py
+++ api/utils.py
@@ -1,11 +1,11 @@
-from django.http import JsonResponse
 from rest_framework import status
+from rest_framework.response import Response
 
 
 class CustomResponseHandler:
     def success_response(self, message: str, object):
         """Generates a standardized success response."""
-        return JsonResponse(
+        return Response(
             {"message": message, "object_id": object.id},
             status=status.HTTP_201_CREATED,
         )
@@ -15,4 +15,4 @@ class CustomResponseHandler:
         response_data = {"error": message}
         if details:
             response_data["details"] = details
-        return JsonResponse(response_data, status=status.HTTP_400_BAD_REQUEST)
+        return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
--- api/views/add_plugin.py
+++ api/views/add_plugin.py
@@ -1,7 +1,7 @@
-from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from api.models.server import Server
@@ -13,19 +13,18 @@ from api.utils import CustomResponseHandler
 class ServersPluginManagementView(APIView, CustomResponseHandler):
     def get(self, request, server_id=None):
         # Attempt to fetch a server by its UUID
-        server = Server.objects.get(id=server_id)
-        if not server.plugins:
-            return JsonResponse(
-                {"message": "No plugins found for the given server"},
-                status=status.HTTP_200_OK,
-            )
+        server = get_object_or_404(Server, pk=server_id)
+
+        # If no plugins are associated with the server
+        if not server.plugins.exists():
+            return self.error_response("No plugins found for the given server")
 
         serialized_plugins = PluginSerializer(server.plugins, many=True)
 
-        return JsonResponse(
+        return Response(
             {
                 "message": f"List of plugin for server: {server_id}",
-                "objects": str(serialized_plugins.data),
+                "objects": serialized_plugins.data,
             },
             status=status.HTTP_200_OK,
         )
@@ -34,6 +33,14 @@ class ServersPluginManagementView(APIView, CustomResponseHandler):
     def post(self, request, server_id=None):
         # Handle the POST logic here if needed
         server = get_object_or_404(Server, pk=server_id)
+
+        # Validate if the plugin is already added to the server
+        plugin_id = request.data.get("plugin")
+        if not plugin_id:
+            return self.error_response("Plugin ID is required")
+
+        if server.plugins.filter(id=plugin_id).exists():
+            return self.error_response("Plugin already added to the server")
 
         serializer = AddPluginSerializer(data=request.data, context={"server": server})
         if serializer.is_valid():
