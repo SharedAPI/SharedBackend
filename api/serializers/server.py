59681d3b614db943cc3d80fbc4f211be6781dccf
Samuel/Bagra <65661423+Bagra67@users.noreply.github.com>
2025-03-26 09:20:02 +0100

    feat: Add token on plugin server tuple (#4)
    Pre commit configurationb has been modified due to windaube error
    ---------
    
    Co-authored-by: sladweinRDR <sladwein@roederer.fr>

--- api/serializers/server.py
+++ api/serializers/server.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
+from django.utils import timezone
 
 from api.models.plugin import Plugin
-from api.models.server import Server
+from api.models.server import Server, ServerPlugins
+
 
 # Serializers define the API representation.
 class ServerSerializer(serializers.ModelSerializer):
@@ -12,9 +14,14 @@ class ServerSerializer(serializers.ModelSerializer):
 
 class AddPluginSerializer(serializers.Serializer):
     plugin = serializers.PrimaryKeyRelatedField(queryset=Plugin.objects.all())
+    start_date = serializers.DateTimeField(default=timezone.now())
 
     def create(self, validated_data):
         server = self.context["server"]
         plugin = validated_data["plugin"]
-        server.plugins.add(plugin)
-        return {"server": server.id, "plugin": plugin.id}
+        start_date = validated_data["start_date"]
+        server_plugin, _ = ServerPlugins.objects.get_or_create(
+            server=server, plugin=plugin, start_date=start_date
+        )
+
+        return server_plugin
